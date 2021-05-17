307c7b3cdd4e892563a7116e226999903b851934
ilya_Smilezz_Chainikov <81897699+Smi1ezz@users.noreply.github.com>
2021-05-17 23:04:20 +0400

    Add complete ex
    Add complete ex

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    NSMutableArray *arrayOfBinary = [[NSMutableArray <NSNumber *> alloc] init];
+    NSInteger maxPow = 128;
+    
+    for (int i = 0; i < 8; i++) {
+        [arrayOfBinary addObject: @(n % 2)];
+        n = n / 2;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        result = result + ([arrayOfBinary[i] intValue] * maxPow);
+        maxPow = maxPow / 2;
+    }
+    return result;
 }
